5367d0694ddf477004151a841c48c2903dfb081d
GoSpoof <gospoof812@gmail.com>
2025-05-28 14:26:22 -0600

    Quartz sync: May 28, 2025, 2:26 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "GoSpoof Docs",
+    pageTitleSuffix: "GoSpoof Docs",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
